e56dd86fef4b06df8fe8bf4370c08eaf87fff1da
paaoogh <paaoogh@gmail.com>
2020-12-14 18:38:55 -0600

    mysql dump

---
+++ Patzcuaro.sql
@@ -0,0 +1,109 @@
+-- MariaDB dump 10.18  Distrib 10.5.8-MariaDB, for osx10.15 (x86_64)
+--
+-- Host: localhost    Database: Patzcuaro
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `nombre` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `apellido1` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `apellido2` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `rfc` varchar(13) NOT NULL,
+  PRIMARY KEY (`rfc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES ('María ','Estévez','Real','ESRM060986QW1'),('Juan','Pérez','Jiménez','PEJJ050467TR8'),('Rafael','Trujillo','Gómez','TRGR040246HY8');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `direcciones`
+--
+
+DROP TABLE IF EXISTS `direcciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `direcciones` (
+  `id_dir` int NOT NULL AUTO_INCREMENT,
+  `calle` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `numero` varchar(5) DEFAULT NULL,
+  `colonia` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `cp` int DEFAULT NULL,
+  `rfc` varchar(13) NOT NULL,
+  PRIMARY KEY (`id_dir`),
+  KEY `rfc` (`rfc`),
+  CONSTRAINT `direcciones_ibfk_1` FOREIGN KEY (`rfc`) REFERENCES `cliente` (`rfc`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `direcciones`
+--
+
+LOCK TABLES `direcciones` WRITE;
+/*!40000 ALTER TABLE `direcciones` DISABLE KEYS */;
+INSERT INTO `direcciones` VALUES (1,'Real Campestre','356','Independencia',61500,'PEJJ050467TR8'),(2,'Carlota Juárez','1236','Ejemplificada',61700,'ESRM060986QW1'),(3,'Azul','23','Las Huertas',61400,'TRGR040246HY8');
+/*!40000 ALTER TABLE `direcciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Ejemplo`
+--
+
+DROP TABLE IF EXISTS `Ejemplo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Ejemplo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rfc` varchar(13) NOT NULL,
+  `campo1` float DEFAULT NULL,
+  `campo2` varchar(21800) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `campo3` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rfc` (`rfc`),
+  CONSTRAINT `ejemplo_ibfk_1` FOREIGN KEY (`rfc`) REFERENCES `cliente` (`rfc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ejemplo`
+--
+
+LOCK TABLES `Ejemplo` WRITE;
+/*!40000 ALTER TABLE `Ejemplo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Ejemplo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
